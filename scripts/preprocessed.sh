21fb23a0e9ee22dc4a09875378b81ed84b07c062
xingran815 <xingran_wang@icloud.com>
2025-11-10 01:44:35 +0000

    add preprocessing codes

--- scripts/preprocessed.sh
+++ scripts/preprocessed.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
 # =============================================================================
 # This script preprocessed.sh runs the program src/preprocessed.py
 # and logs the execution details in the log file
 # logs/preprocessed.logs.
 # =============================================================================
+
+this_dir=$(dirname "$0")
+script_dir="${this_dir}/../src"
+
+python3 "${script_dir}/preprocessed.py"
--- src/preprocessed.py
+++ src/preprocessed.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 -------------------------------------------------------------------------------
 This script `preprocessed.py` retrieves data from the latest CSV file created 
@@ -15,3 +17,47 @@ in the 'data/raw/' directory.
 Any errors or anomalies are also logged to ensure traceability.
 -------------------------------------------------------------------------------
 """
+import os
+from datetime import datetime, timezone
+
+
+# get the current utc time
+current_utc = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+this_dir = os.path.dirname(os.path.abspath(__file__))
+# find out the latest modified file (except sales_data.csv) in data/raw/
+raw_dir = os.path.join(this_dir, "../data/raw/")
+files = os.listdir(raw_dir)
+files = [f for f in files if f.endswith(".csv") and f != "sales_data.csv"]
+latest_file = max(files, key=lambda x: os.path.getmtime(os.path.join(raw_dir, x)))
+
+# 1. It applies preprocessing to the data.
+# read the model and sales value, make them a dictionary
+sales = {}
+with open(os.path.join(raw_dir, latest_file), "r") as f:
+    next(f)  # skip header line
+    for line in f:
+        line = line.strip()
+        if line:
+            timestamp, model, sales_value = line.split(",")
+            if int(sales_value) < 0:
+               sales_value = 0
+            sales[model] = int(sales_value)
+
+# 2. Save the processed data to a new CSV file in data/processed/
+processed_dir = os.path.join(this_dir, "../data/processed/")
+# create this folder if not exist
+os.makedirs(processed_dir, exist_ok=True)
+# create the processed file name
+processed_file = "sales_processed_" + latest_file.split(".")[0][6:] + ".csv"
+processed_file = os.path.join(processed_dir, processed_file)
+# write the processed data to the file
+with open(processed_file, "w") as f:
+    f.write("model,sales\n")
+    for model, sales_value in sales.items():
+        f.write(f"{model},{sales_value}\n")
+
+# 3. Log the preprocessing activity in the logs/preprocessed.logs file.
+log_file = os.path.join(this_dir, "../logs/preprocessed.logs")
+with open(log_file, "a") as f:
+    f.write(f"[{current_utc}] Preprocessed {latest_file} to {processed_file.split("/")[-1]}\n")
+
